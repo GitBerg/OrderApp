dff9e8340d25a2602510542929fe20c971d4945a
GitBerg <beguinhoboy@hotmail.com>
2022-10-17 03:07:40 -0300

    create products card

--- src/pages/NewOrder/index.js
+++ src/pages/NewOrder/index.js
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
-import { View, Text, TextInput, TouchableOpacity, FlatList, Keyboard, TouchableWithoutFeedback, Image } from "react-native";
+import { View, Text, TextInput, TouchableOpacity, FlatList, Keyboard, TouchableWithoutFeedback, Image, KeyboardAvoidingView, Platform } from "react-native";
+import { FontAwesome } from "@expo/vector-icons"
 
 import { ImagesAssets } from '../../../assets/ImagesAssets';
 
@@ -11,15 +12,21 @@ export default function NewOrder({ navigation }) {
 
     const [mesa, setMesa] = useState(null);
     const [produtos, setProdutos] = useState([]);
+    const [observacoes, setObservacoes] = useState(null);
+    const [value, setValue] = useState([]);
+
     // const [finalizado, setFinalizado] = useState(false);
 
     useEffect(() => {
         database.collection("/Users/PHc3F9Pjnw6Fg12SUlKE/Store/").onSnapshot((query) => {
             const list = []
+            
             query.forEach(element => {
                 list.push(element.data().menu)
+
             });
             setProdutos(list)
+            
         })
     }, [])
 
@@ -28,15 +35,20 @@ export default function NewOrder({ navigation }) {
     function addOrder() {
         database.collection("Orders").add({
             mesa: mesa,
+            observacoes: observacoes,
             date: new Date(),
             finalizado: false
         })
 
-        navigation.navigate()
+        navigation.navigate("Order Notes")
     }
 
     return (
-            <View style={styles.container}>
+        <KeyboardAvoidingView style={styles.container}
+            behavior={Platform.OS == "ios" ? "padding" : "height"}
+            keyboardVerticalOffset={10}
+        >
+            <View style={{ width: "100%", height: "95%" }}>
                 <Text style={styles.title}>Novo Pedido</Text>
                 <Text style={styles.description}>Mesa:</Text>
 
@@ -50,17 +62,50 @@ export default function NewOrder({ navigation }) {
                 <Text style={styles.description}>Produtos:</Text>
                 <FlatList
                     showsVerticalScrollIndicator={false}
+                    style={styles.orderMenu}
                     data={produtos}
-                    renderItem={({ item  }) => {
+                    renderItem={({ item }) => {
                         return (
                             <View>
                                 {item.map((el, index) => {
-                                    return(
-                                    <View  key={index}>
-                                        <Text style={{ fontWeight: "bold" }}>{index <= 9 ? "0" + (index + 1) : index}</Text>
-                                        <Text style={{ fontWeight: "bold" }}>{el.name}</Text>
-                                        <Text style={{ fontWeight: "bold" }}>{"R$" + el.price}</Text>
-                                    </View>
+                                    let qtd = 0
+                                    return (
+                                        <View key={index} style={styles.card}>
+                                            <View style={styles.cardDescription}>
+                                                <Text style={{ fontWeight: "bold", fontSize: 18 }}>{el.name}</Text>
+                                                <Text style={{ fontWeight: "bold" }}>{"R$" + el.price}</Text>
+                                            </View>
+                                            <View style={styles.qtd}>
+                                                <Text>Qtd:</Text>
+                                                <View style={styles.btns}>
+                                                    <TouchableOpacity
+                                                        onPress={() =>{
+                                                            
+                                                            setValue(value => value-=1)
+                                                        }}
+                                                    >
+                                                        <FontAwesome
+                                                            name="minus-circle"
+                                                            size={30}
+                                                            color={"#f92e6a"}
+                                                        >
+                                                        </FontAwesome>
+                                                    </TouchableOpacity>
+                                                    <Text>{qtd}</Text>
+                                                    <TouchableOpacity
+                                                      onPress={() =>{
+                                                        setValue(value => value+=1)
+                                                    }}>
+                                                        <FontAwesome
+                                                            name="plus-circle"
+                                                            size={30}
+                                                            color={"#f92e6a"}
+                                                        >
+                                                        </FontAwesome>
+                                                    </TouchableOpacity>
+                                                </View>
+                                            </View>
+                                        </View>
                                     )
                                 })}
                             </View>
@@ -69,14 +114,23 @@ export default function NewOrder({ navigation }) {
                     }
                 />
                 <Text style={styles.description}>Observações:</Text>
+                <TextInput
+                    style={styles.inputObservation}
+                    placeholder="Digite as observacoes do pedido"
+                    onChangeText={setObservacoes}
+                    value={observacoes}
+                    multiline={true}
+                    numberOfLines={4}
+                />
                 <TouchableOpacity
                     style={styles.buttonNewOrder}
                     onPress={() => {
                         addOrder()
                     }}
                 >
-                    <Text>Adicionar</Text>
+                    <Text style={styles.iconButton} >Adicionar</Text>
                 </TouchableOpacity>
             </View>
+        </KeyboardAvoidingView>
     )
 }
--- src/pages/NewOrder/style.js
+++ src/pages/NewOrder/style.js
@@ -25,8 +25,69 @@ const styles = StyleSheet.create({
         borderWidth: 1,
         padding: 5,
         paddingLeft: 12,
-        borderRadius: 4
+        borderRadius: 4,
+        borderColor: "#e5e5e5",
+        backgroundColor: "#f5f5f5cf",
     },
+    card:{
+        borderColor: "#e5e5e5",
+        borderWidth: 1,
+        borderRadius: 4,
+        width: "100%",
+        backgroundColor: "#f5f5f5cf",
+        marginBottom: 10,
+        paddingHorizontal: 14,
+        paddingVertical: 12
+    },
+    cardDescription:{
+        flex: 1,
+        flexDirection: "row",
+        justifyContent: "space-between",
+        marginBottom: 18,
+        alignItems: "center"
+    },
+    qtd:{
+        flex: 1,
+        flexDirection: "row",
+        justifyContent: "space-between",
+        alignItems: "center",
+    },
+    btns:{
+        width:"30%",
+        flexDirection: "row",
+        alignItems: "center",
+        borderWidth: 1,
+        borderColor: "#f92e6a80",
+        padding: 8,
+        justifyContent: "space-between"
+
+    },
+    inputObservation:{
+        fontSize: 18,
+        borderWidth: 1,
+        padding: 5,
+        paddingLeft: 12,
+        borderRadius: 4,
+        backgroundColor: "#f5f5f5cf",
+        borderColor: "#e5e5e5",
+    },
+    buttonNewOrder: {
+        position: "relative",
+        marginTop: 10,
+        width: "40%",
+        padding: 10,
+        marginHorizontal: "30%",
+        backgroundColor: "#ff813e",
+        borderRadius: 14,
+        justifyContent: "center",
+        alignItems: "center"
+
+    },
+    iconButton:{
+        color: "#fff",
+        fontSize: 25,
+        fontWeight: "bold"
+    }
 });
 
 export default styles
